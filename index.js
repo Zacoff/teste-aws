fdc4ec73fed2bba92530029b3f9e4012c3a0370a
Daniel <Daniel.dfa81@gmail.com>
2021-07-26 15:36:56 -0300

    atualizou

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ app.get('/', (req, res) => {
   return res.json({message: 'Server is runnig'})
 })
 
+app.get('/atualizou', (req, res) => {
+  return res.json({message: 'atualizou'})
+})
+
 app.listen(3000, () => {
   console.log('listening on port 3000')
 });
